2ccbb2ce68f545fa7de5897cfa976a251c52d0ee
Michal Janiec <michal.janiec95@gmail.com>
2020-07-30 21:53:37 +0200

    Move label, select, input, button to separate conponents and import them to Form conponent

--- src/Form/index.js
+++ src/Form/index.js
@@ -1,37 +1,27 @@
 import React from "react";
 import "./style.css";
+import Label from "../Label";
+import Select from "../Select";
+import Amount from "../Amount";
+import Button from "../Button";
 
 const Form = () => (
     <form className="form">
         <fieldset className="form__fieldset">
             <legend className="form__legend">Currency coverter input data</legend>
             <p className="flexbox">
-                <label className="form__label" for="select">From currency:</label>
-                <select className="form__select form__select--firstSelect" id="select">
-                    <option></option>
-                    <option>EUR</option>
-                    <option>USD</option>
-                    <option>CHF</option>
-                    <option>GBP</option>
-                    <option>PLN</option>
-                </select>
+                <Label title="From currency:" inputName="fromCurrency" />
+                <Select modifierClass="form__select--firstSelect" selectName="fromCurrency" />
             </p>
             <p className="flexbox">
-                <label className="form__label" for="select">To currency:</label>
-                <select className="form__select" id="select">
-                    <option></option>
-                    <option>EUR</option>
-                    <option>USD</option>
-                    <option>CHF</option>
-                    <option>GBP</option>
-                    <option>PLN</option>
-                </select>
+                <Label title="To currency:" inputName="toCurrency" />
+                <Select selectName="toCurrency" />
             </p>
             <p className="flexbox">
-                <label className="form__label" for="amount">Amount to convert:</label>
-                <input type="number" className="form__input" min="1" step="0.01" />
+                <Label title="Amount to convert:" inputName="amount" />
+                <Amount />
             </p>
-            <button className="form__button">Exchange!</button>
+            <Button />
         </fieldset>
     </form>
 )
