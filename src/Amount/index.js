ea853ad0bcfd1648b994819c1b404c0feeffa5be
Michal Janiec <michal.janiec95@gmail.com>
2020-07-30 21:51:10 +0200

    Move amount input to separete conponent

---
+++ src/Amount/index.js
@@ -0,0 +1,12 @@
+import React from "react";
+import "./style.css";
+
+
+const Amount = () => (
+    <input type="number" className="form__input" id="amount" min="1" step="0.01" />
+);
+
+
+
+export default Amount;
+
