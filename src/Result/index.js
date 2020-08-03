8cecd9b2489daf37b50bc176372c576087276449
Michal Janiec <michal.janiec95@gmail.com>
2020-08-03 21:37:37 +0200

    Render values which are calculate by app. Add style for result span element

--- src/Result/index.js
+++ src/Result/index.js
@@ -1,10 +1,18 @@
 import React from "react";
 import "./style.css";
 
-const Result = () => (
+const Result = ({ resultValue, toCurrencySymbol, amountValue, fromCurrencySymbol }) => {
+  return (
     <section className="section">
-        <h2 className="section__header">Here you see results:</h2>
+      <h2 className="section__header">Here you see result:
+        <span className="section__result">
+          {amountValue} {fromCurrencySymbol}
+          {resultValue !== "" ? " = " : " "}
+          {resultValue} {toCurrencySymbol}
+        </span>
+      </h2>
     </section>
-)
+  )
+};
 
 export default Result;
