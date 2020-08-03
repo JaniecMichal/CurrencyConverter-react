74e56cbe2d8e7e50f09199edbaea1d8279433d4a
Michal Janiec <michal.janiec95@gmail.com>
2020-08-03 21:27:49 +0200

    Made from input amout element controlled conponents.

--- src/Amount/index.js
+++ src/Amount/index.js
@@ -1,12 +1,18 @@
 import React from "react";
 import "./style.css";
 
-
-const Amount = () => (
-    <input type="number" className="form__input" id="amount" min="1" step="0.01" />
+const Amount = ({ stateValue, onChange }) => (
+  <input
+    type="number"
+    className="form__input"
+    id="amount"
+    min="1"
+    step="0.01"
+    value={stateValue}
+    onChange={onChange}
+    required
+  />
 );
-
-
 
 export default Amount;
 
