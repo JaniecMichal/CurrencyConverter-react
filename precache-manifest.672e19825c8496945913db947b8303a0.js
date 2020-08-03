fbb0f8bf6024f80675d8f052d87f9a43255d1333
Michal Janiec <michal.janiec95@gmail.com>
2020-08-03 21:59:30 +0200

    Updates

---
+++ precache-manifest.672e19825c8496945913db947b8303a0.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "03895a25a99b8a930cffdf7fab1f49c5",
+    "url": "/CurrencyConverter-react/index.html"
+  },
+  {
+    "revision": "eb8b10777f75c76442d6",
+    "url": "/CurrencyConverter-react/static/css/main.d2c3f67e.chunk.css"
+  },
+  {
+    "revision": "cb70966e9e6cb160b785",
+    "url": "/CurrencyConverter-react/static/js/2.59662f4b.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/CurrencyConverter-react/static/js/2.59662f4b.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "eb8b10777f75c76442d6",
+    "url": "/CurrencyConverter-react/static/js/main.a805c306.chunk.js"
+  },
+  {
+    "revision": "76725af106054305ec18",
+    "url": "/CurrencyConverter-react/static/js/runtime-main.7eb402b1.js"
+  },
+  {
+    "revision": "79f7fce887ce7316f2d425353e7834c5",
+    "url": "/CurrencyConverter-react/static/media/background-image.79f7fce8.jpg"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/toDO-list-react/precache-manifest.d33f664dd6716d7490560b7d1149e546.js"
+  "/CurrencyConverter-react/precache-manifest.672e19825c8496945913db947b8303a0.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/toDO-list-react/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/CurrencyConverter-react/index.html"), {
   
   blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
 });
